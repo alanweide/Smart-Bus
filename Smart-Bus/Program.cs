1c9dc2d7ce42cd46aa2a75ce5528232d7b8ef4e5
Alan Weide <alan.d.weide@gmail.com>
2019-03-29 13:37:36 -0400

    Initialized VS2013 Project
    Created VS2013 Project in github repo. No code.

---
+++ Smart-Bus/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Smart_Bus
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+        }
+    }
+}
