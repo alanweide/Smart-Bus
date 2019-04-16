69fb5c0a3d018b7581d212c4f277c7db3e2c841e
punkzeta <49076496+punkzeta@users.noreply.github.com>
2019-04-16 14:26:26 -0400

    Update Program.cs

--- Bus-Stop/Program.cs
+++ Bus-Stop/Program.cs
@@ -3,11 +3,48 @@ using Microsoft.SPOT;
 
 namespace Smart_Bus
 {
-    public class Program
+    class BusStopTest
     {
+
         public static void Main()
         {
-            Debug.Print(Resources.GetString(Resources.StringResources.String1));
+            Request[] requests = 
+            { 
+                new Request(0, 10, 1, 5), 
+                new Request(1, 12, 3, 4)
+            };
+            BusStop stop_1 = new BusStop(1);
+
+            Bus[] bus =
+            {
+                new Bus(1, 0, 0, 50),
+                new Bus(2, 0, 0, 50)
+            };
+
+            //update bus1 info to stop1
+            stop_1.update_receive(bus[0], true);
+
+            //assing request to stop1
+            stop_1.request_receive(requests[0]);
+
+            //update bus1 info to stop1
+            bus[0].routeInfo = new Request_v[2];
+            bus[0].routeInfo[0].earliestServingTime = 0;
+            bus[0].routeInfo[0].latestServingTime = 6;
+            bus[0].routeInfo[0].is_origin = true;
+            bus[0].routeInfo[0].location = 1;
+            bus[0].routeInfo[0].served = false;
+
+            bus[0].routeInfo[1].earliestServingTime = 4;
+            bus[0].routeInfo[1].latestServingTime = 10;
+            bus[0].routeInfo[1].is_origin = false;
+            bus[0].routeInfo[1].location = 5;
+            bus[0].routeInfo[1].served = false;
+            stop_1.update_receive(bus[0], true);
+
+            //update bus1 info to stop1
+            stop_1.update_receive(bus[1], true);
+            stop_1.request_receive(requests[1]);
         }
     }
 }
